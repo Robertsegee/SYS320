1da8a3f6eb7faf0f401e8df363addc7328beb9b7
Robertsegee <robert.segee@mymail.champlain.edu>
2021-10-28 17:04:42 -0600

    week08 completed

---
+++ Week08/homework/part2.ps1
@@ -0,0 +1,16 @@
+﻿# Storyline: Review the security event logs
+
+#Directory to save files
+$mydir = "C:\Users\Administrator\Desktop\"
+
+# List all the available winodws event logs
+Get-EventLog -list 
+
+# Create a prompt to allow users to select the log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above"
+
+# Create a prompt to read a string that users can search for
+$readstring = Read-Host -Prompt "Please input a keyword that you would like to search"
+
+# Print the results for the log
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike "*$readstring*" } | export-csv -NoTypeInformation -Path "$mydir\securitylogs.csv"
