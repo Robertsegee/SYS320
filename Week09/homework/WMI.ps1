a4e732756c033698b05800f28e9a184afeaa930a
Robertsegee <robert.segee@mymail.champlain.edu>
2021-12-16 17:01:25 -0700

    finished week 9

---
+++ Week09/homework/WMI.ps1
@@ -0,0 +1,19 @@
+﻿# Use the Get-WMI cmdlet 
+#Get-WmiObject -Class Win32_service | select Name, PathName, ProcessId
+
+#Get-WmiObject -list | where { $_.Name -ilike "Win32_[n-o]*" } | Sort-Object
+
+#Get-WmiObject -Class Win32_account | Get-Member
+
+#task: Grab the network adapter information using the wmi class
+#Get the ip address, default gateway, and the dns server
+
+Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled=$true" | Select DHCPServer
+
+
+
+#Normally this would work however calculator is not installed on this box
+Start-Process -FilePath calc.exe
+Start-Sleep -Seconds 3
+Get-Process -Name Calculator | Stop-Process
+
